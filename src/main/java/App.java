aaa20be944d5509675741c4583b1e3c524e96ebf
ddelsere <delseredelfina@gmail.com>
2023-06-25 12:55:21 -0300

    Ejercicio 2.7

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,25 @@
+import org.uade.algo2.Set;
 import org.uade.algo2.ejercicio6.item3.DoubleLinkedQueue;
+import org.uade.algo2.ejercicio7.Ejercicio7;
+
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
-        DoubleLinkedQueue queue = new DoubleLinkedQueue();
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
-        System.out.println();
-        queue.remove();
-        System.out.println();
+//        DoubleLinkedQueue queue = new DoubleLinkedQueue();
+//        queue.add(1);
+//        queue.add(2);
+//        queue.add(3);
+//        System.out.println();
+//        queue.remove();
+//        System.out.println();
+        int[][] matrix = {{0,0,1,1,0},
+                        {1,0,0,0,0},
+                        {0,1,0,0,0},
+                        {0,0,0,0,1},
+                        {0,0,0,0,0}};
+        List<Set> res = Ejercicio7.grafosFuertementeConexos(matrix);
+        System.out.println("-");
     }
 }
---
+++ src/main/java/org/uade/algo2/ejercicio7/Ejercicio7.java
@@ -0,0 +1,64 @@
+package org.uade.algo2.ejercicio7;
+
+import org.uade.algo2.Set;
+import org.uade.algo2.Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio7 {
+
+    public static List<Set> grafosFuertementeConexos(int[][]matriz){
+        Stack stack = new Stack();
+        int cantNodos = matriz.length;
+        boolean[] visitados = new boolean[cantNodos];
+        for (int i = 0; i < cantNodos; i++) {
+            if(!visitados[i]){
+                dfs(i, visitados, stack, matriz);
+            }
+        }
+        int[][] traspuesta = trasponerMatriz(matriz);
+        visitados = new boolean[cantNodos];
+        List<Set> res = new ArrayList<>();
+        while(!stack.isEmpty()){
+            int vertice = stack.getTop();
+            stack.remove();
+            if(!visitados[vertice]){
+                Set componente = new Set();
+                dfsTraspuesta(vertice, visitados,componente, traspuesta);
+                res.add(componente);
+            }
+        }
+        return res;
+    }
+    
+    private static void dfs(int vertice, boolean[] visitados, Stack stack, int[][]matriz){
+        visitados[vertice] = true;
+        for (int vecino: matriz[vertice]) {
+            if(!visitados[vecino])
+                dfs(vecino, visitados, stack, matriz);
+        }
+        stack.add(vertice);
+    }
+
+    private static void dfsTraspuesta(int vertice, boolean[] visitados, Set componente, int[][] matriz){
+        visitados[vertice] = true;
+        componente.add(vertice);
+        for(int vecino : matriz[vertice]){
+            if(!visitados[vecino]){
+                dfsTraspuesta(vecino, visitados, componente, matriz);
+            }
+        }
+    }
+
+
+//para matriz cuadrada
+    public static int[][] trasponerMatriz(int [][] matriz){
+        int[][] res = new int[matriz.length][matriz.length];
+        for (int i = 0; i < matriz.length; i++)
+            for (int j = 0; j < matriz[0].length; j++)
+                res[j][i] = matriz[i][j];
+        return res;
+    }
+
+}
